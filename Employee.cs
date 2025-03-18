2c558f5dee6013f6b389069d4ed2c0a493ef0d22
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-03-18 09:26:47 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace CSHARP25031801;
+
+class Employee
+{
+    public string Name { get; set; }
+    public string Sex { get; set; }
+    public string Department { get; set; }
+    public int Entry { get; set; }
+    public int Payment { get; set; }
+
+    public override string ToString() =>
+        $"\t Name: {Name}\n" +
+        $"\t Sex: {Sex}\n" +
+        $"\t Department: {Department}\n" +
+        $"\t Entry: {Entry}\n" +
+        $"\t Payment: {Payment:N0} HUF";
+
+    public Employee(string row)
+    {
+        var v = row.Split(';');
+        Name = v[0];
+        Sex = v[1];
+        Department = v[2];
+        Entry = int.Parse(v[3]);
+        Payment = int.Parse(v[4]);
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using CSHARP25031801;
+using System.Security.Cryptography;
+using System.Text;
+
+const string PATH = "D:\\PROJECTS\\CSHARP25031801\\src\\berek2020.txt";
+
+List<Employee> employees = [];
+
+using StreamReader sr = new(PATH, Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) employees.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"t3: number of employees: {employees.Count}");
+
+double avgPayment = employees.Average(e => e.Payment);
+Console.WriteLine($"t4: average payment: {avgPayment / 1000:0.0} tHUF");
+
+Console.Write("t5: enter the name of a department: ");
+string depName = Console.ReadLine();
+
+Console.Write("t6: ");
+Employee hPaidEmp = employees
+    .Where(e => e.Department == depName)
+    .MaxBy(e => e.Payment);
+if (hPaidEmp is null) Console.WriteLine(
+    "the department entered does not exist at the company");
+else Console.WriteLine(
+    $"the highest-earning employee in the department:" +
+    $"\n{hPaidEmp}");
+
+Console.WriteLine("t7: statistics:");
+var deps = employees.GroupBy(e => e.Department);
+foreach (var dep in deps)
+    Console.WriteLine($"\t{dep.Key, -15} - {dep.Count()} ppl.");
+
